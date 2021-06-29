d2115604fa8dc31182435195ce4843c87ecc0c3e
Project35 SOC <socet35@asicfab.ecn.purdue.edu>
2021-06-29 16:23:02 -0400

    Fixed spacing issue.

--- src/common.rs
+++ src/common.rs
@@ -10,11 +10,11 @@ pub const GPIO_POSITIVE_EDGE: u32 =     GPIO + 0x10;
 pub const GPIO_NEGATIVE_EDGE: u32 =     GPIO + 0x14;
 pub const GPIO_INTERRUPT_CLEAR: u32 =	GPIO + 0x18;
 pub const GPIO_INTERRUPT_STATUS: u32 =	GPIO + 0x1C;
-pub const GPIOALL_AFTX06: u32 =		    0xFF;
-pub const GPIOALL: u32 = 		        0xFFFFFFFF;
+pub const GPIOALL_AFTX06: u32 =         0xFF;
+pub const GPIOALL: u32 =                0xFFFFFFFF;
 
 pub fn gpion(pin: i32) -> u32 {
-	1 << pin
+    1 << pin
 }
 
 pub fn rounding_division(dividend: u32, divisor: u32) -> u32 {
