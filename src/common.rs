5193fe563577b4136ac4aa0d669225c90165186a
Project35 SOC <socet35@asicfab.ecn.purdue.edu>
2021-06-29 16:20:25 -0400

    Upgraded gpio.rs to use safe rust wrapper and added volatile-register dependency. Constants will now be stored in common.rs.

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,23 @@
-// Common constants
+// Chip Frequency
 pub const CHIP_FREQ: u32 = 100000000;
 
-pub unsafe fn rounding_division(dividend: u32, divisor: u32) -> u32 {
+// GPIO Constants
+pub const GPIO: u32 =                  	0x80000000;
+pub const GPIO_DATA: u32 =             	GPIO + 0x04;
+pub const GPIO_DATA_DIRECTION: u32 =   	GPIO + 0x08;
+pub const GPIO_INTERRUPT_ENABLE: u32 =	GPIO + 0x0C;
+pub const GPIO_POSITIVE_EDGE: u32 =     GPIO + 0x10;
+pub const GPIO_NEGATIVE_EDGE: u32 =     GPIO + 0x14;
+pub const GPIO_INTERRUPT_CLEAR: u32 =	GPIO + 0x18;
+pub const GPIO_INTERRUPT_STATUS: u32 =	GPIO + 0x1C;
+pub const GPIOALL_AFTX06: u32 =		    0xFF;
+pub const GPIOALL: u32 = 		        0xFFFFFFFF;
+
+pub fn gpion(pin: i32) -> u32 {
+	1 << pin
+}
+
+pub fn rounding_division(dividend: u32, divisor: u32) -> u32 {
     if divisor == 0 { return 0; }
     (dividend + (divisor / 2)) / divisor
 }
--- src/gpio.rs
+++ src/gpio.rs
@@ -1,77 +1,90 @@
-// APB Registers
-pub const GPIO: u32 =                  	0x80000000;
-pub const GPIO_DATA: u32 =             	GPIO + 0x04;
-pub const GPIO_DATA_DIRECTION: u32 =   	GPIO + 0x08;
-pub const GPIO_INTERRUPT_ENABLE: u32 =	GPIO + 0x0C;
-pub const GPIO_POSITIVE_EDGE: u32 =     GPIO + 0x10;
-pub const GPIO_NEGATIVE_EDGE: u32 =     GPIO + 0x14;
-pub const GPIO_INTERRUPT_CLEAR: u32 =	GPIO + 0x18;
-pub const GPIO_INTERRUPT_STATUS: u32 =	GPIO + 0x1C;
-pub const GPIOALL_AFTX06: u32 =		    0xFF;
-pub const GPIOALL: u32 = 		        0xFFFFFFFF;
+use volatile_register::{RW};
 
-// General bit-shift
-pub unsafe fn gpion(pin: i32) -> u32 {
-	1 << pin
+pub struct GPIO {
+    p: &'static mut GPIORegisterBlock
 }
 
-// Input
-pub unsafe fn gpio_enable_input(pins: u32) {
-	let mut value: u32 = core::ptr::read_volatile(GPIO_DATA_DIRECTION as *mut u32);
-	value &= !pins;
-	core::ptr::write_volatile(GPIO_DATA_DIRECTION as *mut u32, value);		
+#[repr(C)]
+struct GPIORegisterBlock {
+    pub data:     RW<u32>,
+    pub data_dir: RW<u32>,
+    pub intr_en:  RW<u32>,
+    pub pos_edge: RW<u32>,
+    pub neg_edge: RW<u32>,
+    pub intr_clr: RW<u32>,
+    pub intr_sts: RW<u32>,
 }
 
-pub unsafe fn gpio_read_input(pins: u32) -> u32 {
-	let mut value: u32 = core::ptr::read_volatile(GPIO_DATA as *mut u32);
-	value &= pins;
-	value
-}
+impl GPIO {
+    pub fn new() -> GPIO {
+        GPIO {
+            p: unsafe { &mut *(0x8000_0004 as *mut GPIORegisterBlock) }
+        }
+    }
 
-// Output
-pub unsafe fn gpio_enable_output(pins: u32) {
-	let mut value: u32 = core::ptr::read_volatile(GPIO_DATA_DIRECTION as *mut u32);
-	value |= pins;
-	core::ptr::write_volatile(GPIO_DATA_DIRECTION as *mut u32, value);
-}
+    pub fn enable_input(&mut self, pins: u32) {
+        unsafe {
+            let mut curr: u32 = self.p.data_dir.read();
+            curr &= !pins;
+            self.p.data_dir.write(curr);
+        }
+    }
 
-pub unsafe fn gpio_set_output(pins: u32, pin_outputs: u32) {
-	let mut value: u32 = core::ptr::read_volatile(GPIO_DATA as *mut u32);
-	value &= !pins;
-	value |= pins & pin_outputs;
-	core::ptr::write_volatile(GPIO_DATA as *mut u32, value);
-}
+    pub fn read_input(&self, pins: u32) -> u32 {
+        self.p.data.read() & pins
+    }
 
-// Interrupts
-pub unsafe fn gpio_enable_interrupt_posedge(pins: u32) {
-    let mut value: u32 = core::ptr::read_volatile(GPIO_NEGATIVE_EDGE as *mut u32);
-    value &= !pins;
-    core::ptr::write_volatile(GPIO_NEGATIVE_EDGE as *mut u32, value);
-    value = core::ptr::read_volatile(GPIO_POSITIVE_EDGE as *mut u32);
-    value |= pins;
-    core::ptr::write_volatile(GPIO_POSITIVE_EDGE as *mut u32, value);
-    value = core::ptr::read_volatile(GPIO_INTERRUPT_ENABLE as *mut u32);
-    value |= pins;
-    core::ptr::write_volatile(GPIO_INTERRUPT_ENABLE as *mut u32, value);
-}
+    pub fn enable_output(&mut self, pins: u32) {
+        unsafe {
+            let mut curr: u32 = self.p.data_dir.read();
+            curr |= pins;
+            self.p.data_dir.write(curr);
+        }
+    }
 
-pub unsafe fn gpio_disable_interrupt_posedge(pins: u32) {
-    let mut value: u32 = core::ptr::read_volatile(GPIO_INTERRUPT_ENABLE as *mut u32);
-    value &= !pins;
-    core::ptr::write_volatile(GPIO_INTERRUPT_ENABLE as *mut u32, value);
-    value = core::ptr::read_volatile(GPIO_POSITIVE_EDGE as *mut u32);
-    value &= !pins;
-    core::ptr::write_volatile(GPIO_POSITIVE_EDGE as *mut u32, value);
-}
+    pub fn set_output(&mut self, pins: u32, pin_outputs: u32) {
+        unsafe {
+            let mut curr: u32 = self.p.data.read();
+            curr &= !pins;
+            curr |= pins & pin_outputs;
+            self.p.data.write(curr);
+        }
+    }
 
-pub unsafe fn gpio_clear_interrupt(pins: u32) {
-    let mut value: u32 = core::ptr::read_volatile(GPIO_INTERRUPT_CLEAR as *mut u32);
-    value |= pins;
-    core::ptr::write_volatile(GPIO_INTERRUPT_CLEAR as *mut u32, value);
-}
+    pub fn enable_interrupt_posedge(&mut self, pins: u32) {
+        unsafe {
+            let mut curr: u32 = self.p.neg_edge.read();
+            curr &= !pins;
+            self.p.neg_edge.write(curr);
+            curr = self.p.pos_edge.read();
+            curr |= pins;
+            self.p.pos_edge.write(curr);
+            curr = self.p.intr_en.read();
+            curr |= pins;
+            self.p.intr_en.write(curr);
+        }
+    }
 
-pub unsafe fn gpio_interrupt_status(pins: u32) -> u32 {
-    let mut value: u32 = core::ptr::read_volatile(GPIO_INTERRUPT_STATUS as *mut u32);
-    value &= pins;
-    value
+    pub fn disable_interrupt_posedge(&mut self, pins: u32) {
+        unsafe {
+            let mut curr: u32 = self.p.intr_en.read();
+            curr &= !pins;
+            self.p.intr_en.write(curr);
+            curr = self.p.pos_edge.read();
+            curr &= !pins;
+            self.p.pos_edge.write(curr);
+        }
+    }
+
+    pub fn clear_interrupt(&mut self, pins: u32) {
+        unsafe{
+            let mut curr: u32 = self.p.intr_clr.read();
+            curr |= pins;
+            self.p.intr_clr.write(curr);
+        }
+    }
+
+    pub fn interrupt_status(&self, pins: u32) -> u32{
+        self.p.intr_sts.read() & pins
+    }
 }
