4397717aa71eaed4d818baf222dd59f39a517c23
goldenbergg <ggoldenb@purdue.edu>
2021-06-30 16:29:18 -0400

    Changed function names to be less repetitive.

--- src/pwm.rs
+++ src/pwm.rs
@@ -19,7 +19,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_frequency(&mut self, channel: i32, mut frequency: u32) {
+    pub fn set_frequency(&mut self, channel: i32, mut frequency: u32) {
         unsafe {
             if common::PWM_MAX_FREQ < frequency { frequency = common::PWM_MAX_FREQ; }
             let period: u32 = common::rounding_division(common::CHIP_FREQ, frequency);
@@ -33,7 +33,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_period(&mut self, channel: i32, period: u32) {
+    pub fn set_period(&mut self, channel: i32, period: u32) {
         unsafe {
             match channel {
                 0 => self.p.pwm0_period.write(period),
@@ -42,7 +42,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_duty(&mut self, channel: i32, duty: u32) {
+    pub fn set_duty(&mut self, channel: i32, duty: u32) {
         unsafe {
             match channel {
                 0 => self.p.pwm0_duty.write(duty + common::AFTX06_DUTY_OFFSET),
@@ -51,7 +51,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_disable(&mut self, channel: i32) {
+    pub fn disable(&mut self, channel: i32) {
         unsafe {
             match channel {
                 0 => {
@@ -64,7 +64,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_enable(&mut self, channel: i32) {
+    pub fn enable(&mut self, channel: i32) {
         unsafe {
             match channel {
                 0 => {
@@ -77,7 +77,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_active_high(&mut self, channel: i32) {
+    pub fn set_active_high(&mut self, channel: i32) {
         unsafe {
             match channel {
                 0 => {
@@ -90,7 +90,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_active_low(&mut self, channel: i32) {
+    pub fn set_active_low(&mut self, channel: i32) {
         unsafe {
             match channel {
                 0 => {
@@ -103,7 +103,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_align_left(&mut self, channel: i32) {
+    pub fn set_align_left(&mut self, channel: i32) {
         unsafe {
             match channel {
                 0 => {
@@ -116,7 +116,7 @@ impl PWM {
         }
     }
 
-    pub fn pwm_set_align_center(&mut self, channel: i32) {
+    pub fn set_align_center(&mut self, channel: i32) {
         unsafe {
             match channel {
                 0 => {
