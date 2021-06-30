4c4d9114b84ba684a0bd3a847106ed6725c0f8a5
goldenbergg <ggoldenb@purdue.edu>
2021-06-30 16:16:30 -0400

    Added PWM constants and misc. functions to common.rs. pwm.rs rewritten using safe Rust wrapper.

--- src/common.rs
+++ src/common.rs
@@ -13,8 +13,27 @@ pub const GPIO_INTERRUPT_STATUS: u32 =	GPIO + 0x1C;
 pub const GPIOALL_AFTX06: u32 =         0xFF;
 pub const GPIOALL: u32 =                0xFFFFFFFF;
 
+// PWM Constants
+pub const PWM: u32 =                      0x80010000;
+pub const PWM_PERIOD: u32 =               PWM + 0x00;
+pub const PWM_DUTY: u32 =                 PWM + 0x04;
+pub const PWM_CONTROL: u32 =              PWM + 0x08;
+pub const PWM_CONTROL_DISABLE: u32 =      !(1 << 0);
+pub const PWM_CONTROL_ENABLE: u32 =       1 << 0;
+pub const PWM_CONTROL_ACTIVE_HIGH: u32 =  !(1 << 1);
+pub const PWM_CONTROL_ACTIVE_LOW: u32 =   1 << 1;
+pub const PWM_CONTROL_ALIGN_LEFT: u32 =   !(1 << 2);
+pub const PWM_CONTROL_ALIGN_CENTER: u32 = 1 << 2;
+pub const PWM_CHANNEL_SIZE: u32 =         0x0C;
+pub const PWM_MAX_FREQ: u32 =             CHIP_FREQ / 2;
+pub const AFTX06_DUTY_OFFSET: u32 =       1;
+
 pub fn gpion(pin: i32) -> u32 {
     1 << pin
+}
+
+pub fn pwmn(channel: u32) -> u32 {
+    channel * PWM_CHANNEL_SIZE
 }
 
 pub fn rounding_division(dividend: u32, divisor: u32) -> u32 {
--- src/pwm.rs
+++ src/pwm.rs
@@ -1,74 +1,131 @@
 use crate::common;
+use volatile_register::{RW};
 
-// APB Registers
-pub const PWM: u32 =                      0x80010000;
-pub const PWM_PERIOD: u32 =               PWM + 0x00;
-pub const PWM_DUTY: u32 =                 PWM + 0x04;
-pub const PWM_CONTROL: u32 =              PWM + 0x08;
-pub const PWM_CONTROL_DISABLE: u32 =      !(1 << 0);
-pub const PWM_CONTROL_ENABLE: u32 =       1 << 0;
-pub const PWM_CONTROL_ACTIVE_HIGH: u32 =  !(1 << 1);
-pub const PWM_CONTROL_ACTIVE_LOW: u32 =   1 << 1;
-pub const PWM_CONTROL_ALIGN_LEFT: u32 =   !(1 << 2);
-pub const PWM_CONTROL_ALIGN_CENTER: u32 = 1 << 2;
-pub const PWM_CHANNEL_SIZE: u32 =         0x0C;
-pub const PWM_MAX_FREQ: u32 =             common::CHIP_FREQ / 2;
-pub const AFTX06_DUTY_OFFSET: u32 =       1;
-
-pub unsafe fn pwmn(channel: u32) -> u32 {
-    channel * PWM_CHANNEL_SIZE
+pub struct PWM {
+    p: &'static mut PWMRegisterBlock
 }
 
-pub unsafe fn pwm_set_frequency(channel: u32, mut frequency: u32) {
-    if PWM_MAX_FREQ < frequency { frequency = PWM_MAX_FREQ; }
-    let period : u32 = common::rounding_division(common::CHIP_FREQ, frequency);
-    core::ptr::write_volatile((PWM_PERIOD + channel) as *mut u32, period);
-    core::ptr::write_volatile((PWM_DUTY + channel) as *mut u32, common::rounding_division(period, 2) + AFTX06_DUTY_OFFSET);
+#[repr(C)]
+struct PWMRegisterBlock {
+    pub pwm0_period: RW<u32>,
+    pub pwm0_duty:   RW<u32>,
+    pub pwm0_ctrl:   RW<u32>,
 }
 
-// Period
-pub unsafe fn pwm_set_period(channel: u32, period: u32) {
-   core::ptr::write_volatile((PWM_PERIOD + channel) as *mut u32, period);
-}
+impl PWM {
+    pub fn new() -> PWM {
+        PWM {
+            p: unsafe { &mut *(0x8001_0000 as *mut PWMRegisterBlock) }
+        }
+    }
 
-// Duty
-pub unsafe fn pwm_set_duty(channel: u32, duty: u32) {
-    core::ptr::write_volatile((PWM_DUTY + channel) as *mut u32, duty + AFTX06_DUTY_OFFSET);
-}
+    pub fn pwm_set_frequency(&mut self, channel: i32, mut frequency: u32) {
+        unsafe {
+            if common::PWM_MAX_FREQ < frequency { frequency = common::PWM_MAX_FREQ; }
+            let period: u32 = common::rounding_division(common::CHIP_FREQ, frequency);
+            match channel {
+                0 => {
+                    self.p.pwm0_period.write(period);
+                    self.p.pwm0_duty.write(common::rounding_division(period, 2) + common::AFTX06_DUTY_OFFSET);
+                }
+                _ => { }
+            }
+        }
+    }
 
-// Control
-pub unsafe fn pwm_disable(channel: u32) {
-    let mut value: u32 = core::ptr::read_volatile((PWM_CONTROL + channel) as *mut u32);
-    value &= PWM_CONTROL_DISABLE;
-    core::ptr::write_volatile((PWM_CONTROL + channel) as *mut u32, value);
-}
+    pub fn pwm_set_period(&mut self, channel: i32, period: u32) {
+        unsafe {
+            match channel {
+                0 => self.p.pwm0_period.write(period),
+                _ => { }
+            }
+        }
+    }
 
-pub unsafe fn pwm_enable(channel: u32) {
-    let mut value: u32 = core::ptr::read_volatile((PWM_CONTROL + channel) as *mut u32);
-    value |= PWM_CONTROL_ENABLE;
-    core::ptr::write_volatile((PWM_CONTROL + channel) as *mut u32, value);
-}
+    pub fn pwm_set_duty(&mut self, channel: i32, duty: u32) {
+        unsafe {
+            match channel {
+                0 => self.p.pwm0_duty.write(duty + common::AFTX06_DUTY_OFFSET),
+                _ => { }
+            }
+        }
+    }
 
-pub unsafe fn pwm_set_active_high(channel: u32) {
-    let mut value: u32 = core::ptr::read_volatile((PWM_CONTROL + channel) as *mut u32);
-    value &= PWM_CONTROL_ACTIVE_HIGH;
-    core::ptr::write_volatile((PWM_CONTROL + channel) as *mut u32, value);
-}
+    pub fn pwm_disable(&mut self, channel: i32) {
+        unsafe {
+            match channel {
+                0 => {
+                    let mut curr: u32 = self.p.pwm0_ctrl.read();
+                    curr &= common::PWM_CONTROL_DISABLE;
+                    self.p.pwm0_ctrl.write(curr);
+                }
+                _ => { }
+            }
+        }
+    }
 
-pub unsafe fn pwm_set_active_low(channel: u32) {
-    let mut value: u32 = core::ptr::read_volatile((PWM_CONTROL + channel) as *mut u32);
-    value |= PWM_CONTROL_ACTIVE_LOW;
-    core::ptr::write_volatile((PWM_CONTROL + channel) as *mut u32, value);
-}
+    pub fn pwm_enable(&mut self, channel: i32) {
+        unsafe {
+            match channel {
+                0 => {
+                    let mut curr: u32 = self.p.pwm0_ctrl.read();
+                    curr |= common::PWM_CONTROL_ENABLE;
+                    self.p.pwm0_ctrl.write(curr);
+                }
+                _ => { }
+            }
+        }
+    }
 
-pub unsafe fn pwm_set_align_left(channel: u32) {
-    let mut value: u32 = core::ptr::read_volatile((PWM_CONTROL + channel) as * mut u32);
-    value &= PWM_CONTROL_ALIGN_LEFT;
-    core::ptr::write_volatile((PWM_CONTROL + channel) as *mut u32, value);
-}
+    pub fn pwm_set_active_high(&mut self, channel: i32) {
+        unsafe {
+            match channel {
+                0 => {
+                    let mut curr: u32 = self.p.pwm0_ctrl.read();
+                    curr &= common::PWM_CONTROL_ACTIVE_HIGH;
+                    self.p.pwm0_ctrl.write(curr);
+                }
+                _ => { }
+            }
+        }
+    }
 
-pub unsafe fn pwm_set_align_center(channel: u32) {
-    let mut value: u32 = core::ptr::read_volatile((PWM_CONTROL + channel) as * mut u32);
-    value |= PWM_CONTROL_ALIGN_CENTER;
-    core::ptr::write_volatile((PWM_CONTROL + channel) as *mut u32, value);
+    pub fn pwm_set_active_low(&mut self, channel: i32) {
+        unsafe {
+            match channel {
+                0 => {
+                    let mut curr: u32 = self.p.pwm0_ctrl.read();
+                    curr |= common::PWM_CONTROL_ACTIVE_LOW;
+                    self.p.pwm0_ctrl.write(curr);
+                }
+                _ => { }
+            }
+        }
+    }
+
+    pub fn pwm_set_align_left(&mut self, channel: i32) {
+        unsafe {
+            match channel {
+                0 => {
+                    let mut curr: u32 = self.p.pwm0_ctrl.read();
+                    curr &= common::PWM_CONTROL_ALIGN_LEFT;
+                    self.p.pwm0_ctrl.write(curr);
+                }
+                _ => { }
+            }
+        }
+    }
+
+    pub fn pwm_set_align_center(&mut self, channel: i32) {
+        unsafe {
+            match channel {
+                0 => {
+                    let mut curr: u32 = self.p.pwm0_ctrl.read();
+                    curr |= common::PWM_CONTROL_ALIGN_CENTER;
+                    self.p.pwm0_ctrl.write(curr);
+                }
+                _ => { }
+            }
+        }
+    }
 }
