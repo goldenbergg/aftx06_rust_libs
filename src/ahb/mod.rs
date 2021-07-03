ad3d6cf0fe5d4a148d852a602780d0e300cc507a
goldenbergg <ggoldenb@purdue.edu>
2021-07-02 21:58:57 -0400

    Added plic.rs and added PLIC constants to common.rs.

--- src/ahb/mod.rs
+++ src/ahb/mod.rs
@@ -1,1 +1,2 @@
 pub mod clint;
+pub mod plic;
--- src/common.rs
+++ src/common.rs
@@ -4,6 +4,7 @@ pub const CHIP_FREQ: u32 = 100000000;
 // APB Construction Checker
 pub static mut CLINT_CONSTRUCTED: bool = false;
 pub static mut GPIO_CONSTRUCTED: bool =  false;
+pub static mut PLIC_CONSTRUCTED: bool =  false;
 pub static mut PWM_CONSTRUCTED: bool =   false;
 pub static mut TIM_CONSTRUCTED: bool =   false;
 
@@ -27,6 +28,47 @@ pub const GPIO_INTERRUPT_CLEAR: u32 =	GPIO + 0x18;
 pub const GPIO_INTERRUPT_STATUS: u32 =	GPIO + 0x1C;
 pub const GPIOALL_AFTX06: u32 =         0xFF;
 pub const GPIOALL: u32 =                0xFFFFFFFF;
+
+// PLIC Constants
+pub const PLIC: u32 =        0xE0000000;
+pub const PLIC_RES1: u32 =   PLIC + 0x00;
+pub const PLIC_IPR1: u32 =   PLIC + 0x04;
+pub const PLIC_IPR2: u32 =   PLIC + 0x08;
+pub const PLIC_IPR3: u32 =   PLIC + 0x0C;
+pub const PLIC_IPR4: u32 =   PLIC + 0x10;
+pub const PLIC_IPR5: u32 =   PLIC + 0x14;
+pub const PLIC_IPR6: u32 =   PLIC + 0x18;
+pub const PLIC_IPR7: u32 =   PLIC + 0x1C;
+pub const PLIC_IPR8: u32 =   PLIC + 0x20;
+pub const PLIC_IPR9: u32 =   PLIC + 0x24;
+pub const PLIC_IPR10: u32 =  PLIC + 0x28;
+pub const PLIC_IPR11: u32 =  PLIC + 0x2C;
+pub const PLIC_IPR12: u32 =  PLIC + 0x30;
+pub const PLIC_IPR13: u32 =  PLIC + 0x34;
+pub const PLIC_IPR14: u32 =  PLIC + 0x38;
+pub const PLIC_IPR15: u32 =  PLIC + 0x3C;
+pub const PLIC_IPR16: u32 =  PLIC + 0x40;
+pub const PLIC_IPR17: u32 =  PLIC + 0x44;
+pub const PLIC_IPR18: u32 =  PLIC + 0x48;
+pub const PLIC_IPR19: u32 =  PLIC + 0x4C;
+pub const PLIC_IPR20: u32 =  PLIC + 0x50;
+pub const PLIC_IPR21: u32 =  PLIC + 0x54;
+pub const PLIC_IPR22: u32 =  PLIC + 0x58;
+pub const PLIC_IPR23: u32 =  PLIC + 0x5C;
+pub const PLIC_IPR24: u32 =  PLIC + 0x60;
+pub const PLIC_IPR25: u32 =  PLIC + 0x64;
+pub const PLIC_IPR26: u32 =  PLIC + 0x68;
+pub const PLIC_IPR27: u32 =  PLIC + 0x6C;
+pub const PLIC_IPR28: u32 =  PLIC + 0x70;
+pub const PLIC_IPR29: u32 =  PLIC + 0x74;
+pub const PLIC_IPR30: u32 =  PLIC + 0x78;
+pub const PLIC_IPR31: u32 =  PLIC + 0x7C;
+pub const PLIC_IPR32: u32 =  PLIC + 0x80;
+pub const PLIC_IPNDGR: u32 = PLIC + 0x84;
+pub const PLIC_IER: u32 =    PLIC + 0x88;
+pub const PLIC_RES2: u32 =   PLIC + 0x8C;
+pub const PLIC_PTR: u32 =    PLIC + 0x90;
+pub const PLIC_CCRL: u32 =   PLIC + 0x94;
 
 // PWM Constants
 pub const PWM: u32 =                      0x80010000;
